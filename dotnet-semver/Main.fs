1c1ade7426de342136d7fb15772ec2f95ccd4ab2
Matt Davey <matt.davey@fsfe.org>
2023-09-22 22:10:57 +0100

    Add `--force` switch to `init` command

--- dotnet-semver/Main.fs
+++ dotnet-semver/Main.fs
@@ -87,10 +87,10 @@ let format (formatString: string) semver =
 
 let tagFormat = "v%M.%m.%p%s%d"
 
-let init () =
+let init force =
     let filepath = Path.Join(Environment.CurrentDirectory, ".semver")
 
-    if File.Exists(filepath) then
+    if File.Exists(filepath) && not force then
         exitWithMessage ".semver already exists" -1
     else
         save filepath defaultVersion
@@ -116,47 +116,48 @@ let spawn command (args: string list) =
     exit proc.ExitCode
 
 let usage = """
-USAGE: dotnet semver [--help] [init | inc <version> | pre <value> | meta <value> | tag | next <version> | format <format>]
+USAGE: dotnet semver [--help] [init [--force] | inc <version> | pre <value> | meta <value> | tag | next <version> | format <format>]
 
 OPTIONS:
     --help - Display this list of options.
 
 SUBCOMMANDS:
-    init[ialize]          - Initializes a new .semver file with an initial version v0.1.0.
-    inc[rement] <version> - Increments the specified version number according to semver2 rules. <version> must be one of [major|minor|patch].
-    pre[release] <value>  - Sets the pre-release version suffix.
-    meta[data] <value>    - Sets the metadata value.
-    next <version>        - Format incremented specific version without saving it. <version> must be one of [major|minor|patch].
-    tag                   - Print the tag for the current .semver file.
-    format <format>       - Find the .semver file and print a formatted string from this.
+    init[ialize] [--force] - Initializes a new .semver file with an initial version v0.1.0.
+    inc[rement] <version>  - Increments the specified version number according to semver2 rules. <version> must be one of [major|minor|patch].
+    pre[release] <value>   - Sets the pre-release version suffix.
+    meta[data] <value>     - Sets the metadata value.
+    next <version>         - Format incremented specific version without saving it. <version> must be one of [major|minor|patch].
+    tag                    - Print the tag for the current .semver file.
+    format <format>        - Find the .semver file and print a formatted string from this.
     
 DOTNET CLI WRAPPERS:
-    build [args]          - Executes dotnet build, passing the current semver as a switch.
-    pack [args]           - Executes dotnet pack, passing the current semver as a switch.
-    publish [args]        - Executes dotnet publish, passing the current semver as a switch."""
+    build [args]           - Executes dotnet build, passing the current semver as a switch.
+    pack [args]            - Executes dotnet pack, passing the current semver as a switch.
+    publish [args]         - Executes dotnet publish, passing the current semver as a switch."""
 
 [<EntryPoint>]
 let main argv =
     match Array.toList argv with
-    | ["--help"]                     -> exitWithMessage usage 0
-    | ["format"]                     -> exitWithMessage "required: format string" -1
-    | ["format"; fmt]                -> read () |> format fmt |> printfn "%s"
-    | ["inc" | "increment"]          -> exitWithMessage "required: major | minor | patch" -1
-    | ["inc" | "increment"; element] -> modify (increment element)
-    | ["init" | "initialize"]        -> init ()
-    | ["meta" | "metadata"]          -> modify (setMetadata None)
-    | ["meta" | "metadata"; value]   -> modify (setMetadata (Some value))
-    | ["pre" | "prerelease"]         -> modify (setSpecial None)
-    | ["pre" | "prerelease"; value]  -> modify (setSpecial (Some value))
-    | ["spe" | "special"]            -> modify (setSpecial None)
-    | ["spe" | "special"; value]     -> modify (setSpecial (Some value))
-    | ["tag"]                        -> read () |> format tagFormat |> printfn "%s"
-    | ["next"]                       -> exitWithMessage "required: major | minor | patch" -1
-    | ["next"; element]              -> read () |> increment element |> format tagFormat |> printfn "%s"
-    | "build"::args                  -> spawn "build" args
-    | "pack"::args                   -> spawn "pack" args
-    | "publish"::args                -> spawn "publish" args
-    | []                             -> read () |> format tagFormat |> printfn "%s"
-    | _                              -> exitWithMessage usage -1
+    | ["--help"]                         -> exitWithMessage usage 0
+    | ["format"]                         -> exitWithMessage "required: format string" -1
+    | ["format"; fmt]                    -> read () |> format fmt |> printfn "%s"
+    | ["inc" | "increment"]              -> exitWithMessage "required: major | minor | patch" -1
+    | ["inc" | "increment"; element]     -> modify (increment element)
+    | ["init" | "initialize"]            -> init false
+    | ["init" | "initialize"; "--force"] -> init true
+    | ["meta" | "metadata"]              -> modify (setMetadata None)
+    | ["meta" | "metadata"; value]       -> modify (setMetadata (Some value))
+    | ["pre" | "prerelease"]             -> modify (setSpecial None)
+    | ["pre" | "prerelease"; value]      -> modify (setSpecial (Some value))
+    | ["spe" | "special"]                -> modify (setSpecial None)
+    | ["spe" | "special"; value]         -> modify (setSpecial (Some value))
+    | ["tag"]                            -> read () |> format tagFormat |> printfn "%s"
+    | ["next"]                           -> exitWithMessage "required: major | minor | patch" -1
+    | ["next"; element]                  -> read () |> increment element |> format tagFormat |> printfn "%s"
+    | "build"::args                      -> spawn "build" args
+    | "pack"::args                       -> spawn "pack" args
+    | "publish"::args                    -> spawn "publish" args
+    | []                                 -> read () |> format tagFormat |> printfn "%s"
+    | _                                  -> exitWithMessage usage -1
 
     exit 0
